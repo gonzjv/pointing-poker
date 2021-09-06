9be152dc3d38f35e3960e2bdc8f9a788c0770da1
makhitr <ring_ru@yahoo.com>
2021-09-07 00:26:17 +0300

    feat: main section

--- pointing-poker/src/pages/welcome/Welcome.jsx
+++ pointing-poker/src/pages/welcome/Welcome.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import './welcome.css'
+import './welcome.css';
+
+
 
 function Welcome() {
   return (
 
-   
-    <div> sdsd</div>
-    
+
+    <div className="wrapper">
+      <div className="title">
+        <img src="./images/cards.png" alt="cards" />
+        <h1> Poker Planning</h1>
+
+        <section className="form">
+          <h2>Start your planning</h2>
+          <h4>Creat session</h4>
+          <button className="button">Start new game</button>
+          <p>or:</p>
+          <h4>Connect to lobby by URL:</h4>
+          <input></input><button>Connect</button>
+        </section>
+
+      </div>
+    </div>
+
   )
 }
 
