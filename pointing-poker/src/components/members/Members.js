15eb9d1691d77bb5c7f5ff813b0c0ca864804763
Andrey Tarasevich <eansable@yandex.by>
2021-09-07 10:15:04 +0300

    feat: add responsible on members block

--- pointing-poker/src/components/members/Members.js
+++ pointing-poker/src/components/members/Members.js
@@ -10,14 +10,16 @@ const memberInfo = {
 export default function Members() {
   return (
     <div className="members">
-      <h2 className="members__title">members:</h2>
-      <div className="members__list">
-        <MemberCard member={memberInfo} />
-        <MemberCard member={memberInfo} />
-        <MemberCard member={memberInfo} />
-        <MemberCard member={memberInfo} />
-        <MemberCard member={memberInfo} />
-        <MemberCard member={memberInfo} />
+      <div className="wrapper">
+        <h2 className="members__title">members:</h2>
+        <div className="members__list">
+          <MemberCard member={memberInfo} />
+          <MemberCard member={memberInfo} />
+          <MemberCard member={memberInfo} />
+          <MemberCard member={memberInfo} />
+          <MemberCard member={memberInfo} />
+          <MemberCard member={memberInfo} />
+        </div>
       </div>
     </div>
   );
