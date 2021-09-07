ae01f3a046b71e4f7f2262cf9bf1cce2958f6c60
makhitr <ring_ru@yahoo.com>
2021-09-07 16:38:46 +0300

    feat: styled welcome page

--- pointing-poker/src/pages/welcome/Welcome.jsx
+++ pointing-poker/src/pages/welcome/Welcome.jsx
@@ -5,23 +5,20 @@ import './welcome.css';
 
 function Welcome() {
   return (
-
-
-    <div className="wrapper">
-      <div className="title">
-        <img src="./images/cards.png" alt="cards" />
-        <h1> Poker Planning</h1>
-
-        <section className="form">
-          <h2>Start your planning</h2>
-          <h4>Creat session</h4>
-          <button className="button">Start new game</button>
-          <p>or:</p>
-          <h4>Connect to lobby by URL:</h4>
-          <input></input><button>Connect</button>
-        </section>
-
+    <div className="wrapper welcome-wrapper">
+     
+      <section className="main-section">
+      <div className="game-title">
+      <h1 className="title"> <img src="./images/cards.png" alt="cards" /> Poker Planning</h1>
       </div>
+        <h2 className="heading">Start your planning:</h2>
+        <h4 className="form-input">Create session:</h4>
+        <button className="button">Start new game</button>
+        <p className="heading form-text">or:</p>
+        <h4 className="form-input up">Connect to lobby by URL:</h4>
+        <input></input>
+        <button className="button">Connect</button>
+      </section>
     </div>
 
   )
