33047ac9660d2bd2e567f27521f0bf4f628e4cde
makhitr <ring_ru@yahoo.com>
2021-09-09 17:31:02 +0300

    fix: open-close popup, add popup header

--- pointing-poker/src/components/Form/Form.jsx
+++ pointing-poker/src/components/Form/Form.jsx
@@ -1,74 +1,129 @@
-import React, { useState, useEffect } from 'react';
-import './form.css'
+import React, { useState, useEffect } from "react";
+import "./form.css";
+import PropTypes from "prop-types";
 
-function Form({ setFormValues }) {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [jobPosition, setJobPosition] = useState('');
+Form.propTypes = {
+  active: PropTypes.bool,
+  setActive: PropTypes.func
+}
+
+
+function Form({ setFormValues, active, setActive }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [jobPosition, setJobPosition] = useState("");
   const [errors, setErrors] = useState({});
 
   useEffect(() => {
-    validate()
-  }, [firstName])
+    validate();
+  }, [firstName]);
   const validate = () => {
     setErrors({});
-    if (firstName === '') {
+    if (firstName === "") {
       setErrors((state) => ({ ...state, firstName }));
-    };
-    console.log(errors)
-  }
-
+    }
+    console.log(errors);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (Object.keys(errors).length === 0) {
-      setFormValues((state) => [...state, { firstName, lastName, jobPosition },
-      ])
+      setFormValues((state) => [
+        ...state,
+        { firstName, lastName, jobPosition },
+      ]);
     }
     console.log(firstName);
     console.log(lastName);
     console.log(jobPosition);
-  }
+  };
 
   return (
     <>
       <section className="popup">
-        <form onSubmit={handleSubmit}>
+
+        <div className={active ? "modal active" : "modal" } onClick={() => setActive(false)}>
+        <div className="popup_content">
+        <div className="popup_header">
+         <h3>Connect to Lobby</h3>
+         <div className="toggle-wrapper">
+           <p>Connect as<br /> Observer</p>
+
+           <label className="switch">
+             <input type="checkbox" />
+             <span className="slider"></span>
+           </label>
+         </div>
+       </div>
+        <form onSubmit={handleSubmit} onClick={(e) => e.stopPropagation()} >
           <div className="group">
-            <label className="group-input" htmlFor="first-name">Your first name:
-              {errors?.firstName === "" && <span className="form-validation">Enter your name</span>}
+            <label className="group-input" htmlFor="first-name">
+              Your first name:
+              {errors?.firstName === "" && (
+                <span className="form-validation">Enter your name</span>
+              )}
             </label>
-            <input type="text" name="first-name" value={firstName} onChange={(event) => setFirstName(event.target.value)} required />
+            <input
+              type="text"
+              name="first-name"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+              required
+            />
           </div>
           <div className="group">
-            <label className="group-input" htmlFor="last-name">Your last name (optional):</label>
-            <input type="text" name="first-name" value={lastName} onChange={(event) => setLastName(event.target.value)} />
+            <label className="group-input" htmlFor="last-name">
+              Your last name (optional):
+            </label>
+            <input
+              type="text"
+              name="first-name"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+            />
           </div>
           <div className="group">
-            <label className="group-input" htmlFor="job-position">Your job posititon (optional):</label>
-            <input type="text" name="job-position" value={jobPosition} onChange={(event) => setJobPosition(event.target.value)} />
+            <label className="group-input" htmlFor="job-position">
+              Your job posititon (optional):
+            </label>
+            <input
+              type="text"
+              name="job-position"
+              value={jobPosition}
+              onChange={(event) => setJobPosition(event.target.value)}
+            />
           </div>
-          <div className="group" >
+          <div className="group">
             Image:
             {/* <label className="group-input img" htmlFor="file" >Image:</label> */}
             {/* <input type="file" id="image" /> */}
             <div className="group file">
-              <label htmlFor="file" className="group-input"><span className="input-file">Choose a file</span></label>
-              <input type="file" name="file" id="file" className="group-input file" />
+              <label htmlFor="file" className="group-input">
+                <span className="input-file">Choose a file</span>
+              </label>
+              <input
+                type="file"
+                name="file"
+                id="file"
+                className="group-input file"
+              />
               <button className="button">Button</button>
             </div>
+            <div className="avatar">
+              <img src="images/avatar.png" alt="avatar"></img>
 
-
+            </div>
           </div>
-          <div className="button">
-            <input type="submit" value="Confirm" />
+          <div className="form-buttons">
+            <input type="submit" value="Confirm" className="button button-blue" />
+            <input type="submit" value="Cancel" className="button  button-white" />
           </div>
         </form>
+        </div>
+        </div>
       </section>
-
-
     </>
-  )
+  );
 }
 
 export default Form;
--- pointing-poker/src/pages/welcome/Welcome.jsx
+++ pointing-poker/src/pages/welcome/Welcome.jsx
@@ -1,9 +1,14 @@
 import React, {useState} from 'react';
 import Form  from '../../components/Form/Form';
-import './welcome.css';
+import './welcome.css'; 
+
+
 
 function Welcome() {
-  const [formValues, setFormValues] = useState([])
+
+  const [formValues, setFormValues] = useState([]);
+  const [modalActive, setModalActive] = useState(false);
+
 
   return (
     <div className="wrapper welcome-wrapper">
@@ -14,13 +19,13 @@ function Welcome() {
       </div>
         <h2 className="heading">Start your planning:</h2>
         <h4 className="form-input">Create session:</h4>
-        <button className="button">Start new game</button>
+        <button className="button" onClick={() => setModalActive(true)}>Start new game</button>
         <p className="heading form-text">or:</p>
         <h4 className="form-input up">Connect to lobby by <span className="colored">URL</span> :</h4>
         <input></input>
         <button className="button">Connect</button>
       </section>
-      <Form setFormValues={setFormValues} />
+      <Form setFormValues={setFormValues} active={modalActive} setActive={setModalActive}/>
     </div>
 
   )
