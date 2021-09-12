e256111f4790e912062fb0e17e598305b775a471
gonzjv <gonzjv@ya.ru>
2021-09-12 22:17:14 +0300

    refactor: change backend URL to heroku

--- pointing-poker/src/socketContext.js
+++ pointing-poker/src/socketContext.js
@@ -1,16 +1,13 @@
-import React from 'react'
-import io from 'socket.io-client'
+import React from 'react';
+import io from 'socket.io-client';
 
-const SocketContext = React.createContext()
+const SocketContext = React.createContext();
 
 const SocketProvider = ({ children }) => {
-    const ENDPOINT = 'http://localhost:5000'
-    const socket = io(ENDPOINT, { transports: ['websocket', 'polling'] })
-    return (
-        <SocketContext.Provider value={socket}>
-            {children}
-        </SocketContext.Provider>
-    )
-}
+  // const ENDPOINT = 'http://localhost:5000'
+  const ENDPOINT = 'https://pointing-poker-rs2021q3-team10.herokuapp.com/';
+  const socket = io(ENDPOINT, { transports: ['websocket', 'polling'] });
+  return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
+};
 
-export{SocketContext, SocketProvider}
+export { SocketContext, SocketProvider };
