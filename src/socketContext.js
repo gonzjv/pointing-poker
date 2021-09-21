ec0ba3df1739ac07a862818b24f627ea55754505
gonzjv <gonzjv@ya.ru>
2021-09-21 06:02:59 +0300

    refactor: change socket.io URL to heroku BE

--- src/socketContext.js
+++ src/socketContext.js
@@ -4,9 +4,7 @@ import io from 'socket.io-client';
 const SocketContext = React.createContext();
 
 const SocketProvider = ({ children }) => {
-  // const ENDPOINT = 'https://pointing-poker-rs2021q3-team10.herokuapp.com/';
-  // const ENDPOINT = 'http://localhost:5000';
-  const ENDPOINT = 'https://pointing-poker-team10-test.herokuapp.com/';
+  const ENDPOINT = 'https://pointing-poker-rs2021q3-team10.herokuapp.com/';
   const socket = io(ENDPOINT, { transports: ['websocket', 'polling'] });
   return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
 };
