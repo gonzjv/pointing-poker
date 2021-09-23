ab7fce82df85ee3b3d0d4c7d8c3a6bfdf268ba5e
Eansable <50375756+Eansable@users.noreply.github.com>
2021-09-23 23:26:32 +0300

    Feature/lobby page (#7)
    * feat: create block with members
    
    * feat: add responsible on members block
    
    * feat: add block settings without logic and timer
    
    * fix: fixed style in switch
    
    * feat: add timer
    
    * feat: add block with game info
    
    * feat: create block with issues
    
    * feat: create components card
    
    * refactor: move to Lobby components  GameInfo etc.
    
    * refactor: clean unnecessary fragment
    
    * refactor: update react, react-dom
    
    Co-authored-by: gonzjv <gonzjv@ya.ru>

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -3,7 +3,12 @@ import { useHistory } from 'react-router-dom';
 import { MainContext } from '../../mainContext';
 import { SocketContext } from '../../socketContext';
 import { UsersContext } from '../../usersContext';
+import Card from '../card/card';
+import GameInfo from './game-info/game-info';
+import IssuesList from './issues/issues';
 import './Lobby.css';
+import Members from './members/Members';
+import Settings from './settings/settings';
 
 const Lobby = () => {
   const socket = useContext(SocketContext);
@@ -17,6 +22,11 @@ const Lobby = () => {
     history.push('/');
   };
 
+  const cardInfo = {
+    value: '5',
+    type: 'SP',
+  };
+
   return (
     <main>
       <p>
@@ -27,15 +37,18 @@ const Lobby = () => {
       <p>Players: </p>
       {players.map((player) => {
         return (
-          <>
-            <p>
-              {player.firstName} {player.lastName}
-            </p>
-          </>
+          <p>
+            {player.firstName} {player.lastName}
+          </p>
         );
       })}
       <button onClick={handleClick}>Go Home</button>
       <span style={{ fontStyle: 'italic' }}></span>
+      <GameInfo />
+      <Members />
+      <IssuesList />
+      <Settings />
+      <Card card={cardInfo} />
     </main>
   );
 };
