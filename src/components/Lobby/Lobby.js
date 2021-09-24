382a813b894162ef7bcd2ee1b1da70ec7baf1e1d
Max <gonzjv@ya.ru>
2021-09-24 22:04:30 +0300

    Feature/add lobby functionality (#8)
    * feat: add exit from lobby
    
    * feat: player connect notification
    
    * feat: chat

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { MainContext } from '../../mainContext';
 import { SocketContext } from '../../socketContext';
@@ -7,6 +7,8 @@ import Card from '../card/card';
 import GameInfo from './game-info/game-info';
 import IssuesList from './issues/issues';
 import './Lobby.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Members from './members/Members';
 import Settings from './settings/settings';
 
@@ -14,12 +16,42 @@ const Lobby = () => {
   const socket = useContext(SocketContext);
   const { firstName, lastName } = useContext(MainContext);
   const history = useHistory();
-  const { players, dealer, setDealer } = useContext(UsersContext);
+  const { players, dealer, setPlayers, setDealer } = useContext(UsersContext);
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
 
-  const handleClick = () => {
+  const notify = (message) => {
+    toast(message, {
+      position: 'top-center',
+    });
+  };
+
+  useEffect(() => {
+    socket.on('players', (players) => {
+      setPlayers(players);
+    });
+  });
+
+  useEffect(() => {
+    socket.on('message', (msg) => {
+      setMessages((messages) => [...messages, msg]);
+    });
+    socket.on('notification', (message) => {
+      notify(message);
+    });
+  }, [socket]);
+
+  const handleExit = () => {
     setDealer({});
-    console.log('dealer: ', dealer);
-    history.push('/');
+    socket.emit('exit', () => {
+      history.push('/');
+    });
+  };
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    socket.emit('sendMessage', message);
+    setMessage('');
   };
 
   const cardInfo = {
@@ -29,21 +61,55 @@ const Lobby = () => {
 
   return (
     <main>
-      <p>
-        Good day, {firstName} {lastName}!
-      </p>
-      <p>Game ID : {dealer.lobbyID}</p>
-      <p>Dealer : {dealer.firstName}</p>
-      <p>Players: </p>
-      {players.map((player) => {
-        return (
-          <p>
-            {player.firstName} {player.lastName}
-          </p>
-        );
-      })}
-      <button onClick={handleClick}>Go Home</button>
-      <span style={{ fontStyle: 'italic' }}></span>
+      <ToastContainer />
+      <section className="status">
+        <p>
+          Good day, {firstName} {lastName}! Now you are in the lobby with gameID:{' '}
+          {dealer.lobbyID}.
+        </p>
+        <p>Waiting for {dealer.firstName} to start the game</p>
+        <p>Game ID : {dealer.lobbyID}</p>
+        <p>Dealer : {dealer.firstName}</p>
+      </section>
+      <section className="players">
+        <p>Players: </p>
+        {players.map((player) => {
+          return (
+            <>
+              <p>
+                {player.firstName} {player.lastName}
+              </p>
+            </>
+          );
+        })}
+        <button onClick={handleExit}>Exit</button>
+      </section>
+      <section className="chat">
+        <div className="chat-window">
+          {messages.length > 0 ? (
+            messages.map((msg, i) => (
+              <div key={i}>
+                <p>
+                  {msg.user}:<span>{msg.text}</span>
+                </p>
+              </div>
+            ))
+          ) : (
+            <div>----------</div>
+          )}
+        </div>
+        <form className="chat-form" onSubmit={handleSendMessage}>
+          <input
+            type="text"
+            placeholder="Enter message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit" onClick={handleSendMessage}>
+            Send
+          </button>
+        </form>
+      </section>
       <GameInfo />
       <Members />
       <IssuesList />
