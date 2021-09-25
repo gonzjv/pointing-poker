719c3bb7e43c1340ef1569a0eb5d4007206f8ad4
Andrey Tarasevich <eansable@yandex.by>
2021-09-26 02:00:17 +0300

    feat: create component Button and add function copy text ID

--- src/components/Lobby/game-info/game-info.js
+++ src/components/Lobby/game-info/game-info.js
@@ -1,7 +1,23 @@
+import Button from '../../Button/Button';
 import MemberCard from '../members/Member-card/Member-card';
 import './game-info.css';
 
 export default function GameInfo({ dealer }) {
+  const copyID = (event) => {
+    const idArea = document.getElementById('id__text');
+    const id = idArea.value;
+    navigator.clipboard.writeText(id).then(
+      function () {
+        console.log('Async: Copying to clipboard was successful!');
+      },
+      function (err) {
+        console.error('Async: Could not copy text: ', err);
+      },
+    );
+  };
+
+  const emptyFunction = () => {};
+
   return (
     <div className="game-info">
       <h2 className="lobby__subtitle game-info__title">{dealer.lobbyID}</h2>
@@ -11,11 +27,14 @@ export default function GameInfo({ dealer }) {
       </div>
       <div className="link">
         <p>Lobby ID:</p>
-        <div className="link__text">{dealer.lobbyID}</div>
-        <button>Copy</button>
+        <div className="game__id">
+          <input className="link__text" id="id__text" value={dealer.lobbyID} />
+
+          <Button value="Copy" onCustomClick={copyID} />
+        </div>
       </div>
-      <button>Start Game</button>
-      <button className="button__close">Canсel game</button>
+      <Button value="Start Game" />
+      <Button value="Cancel game" isWhite={true} />
     </div>
   );
 }
